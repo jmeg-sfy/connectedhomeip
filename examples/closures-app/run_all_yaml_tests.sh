baf470ba16c6bac3e239a2aa1381fa07ad8725d9
jtov-sfy <jorge.tovar@somfy.com>
2024-10-08 18:33:25 +0200

    [TEST] Add pics file for closures app

--- examples/closures-app/run_all_yaml_tests.sh
+++ examples/closures-app/run_all_yaml_tests.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ## ---
-# This is a helper script that runs all the yaml tests for all the application clusters enabled in the RVC App.
+# This is a helper script that runs all the yaml tests for all the application clusters enabled in the Closures App.
 # Run the script from the root dir.
 # The script takes the node ID that the device was commissioned with.
 ## ---
 
 NODEID=$1
-RVC_DEVICE_ENDPOINT=1
-RVC_DEVICE_PICS="examples/closures-app/closures-common/pics/closures-app-pics-values"
+CLOSURES_DEVICE_ENDPOINT=1
+CLOSURES_DEVICE_PICS="examples/closures-app/closures-common/pics/closures-app-pics-values"
 
 if [ -z "$NODEID" ]; then
     echo "Usage: run_all_yaml_tests [Node ID]"
@@ -16,5 +16,5 @@ if [ -z "$NODEID" ]; then
 fi
 
 # Closures Operational State cluster
-./scripts/tests/chipyaml/chiptool.py tests Test_TC_CLOPSTATE_1_1 --PICS "$RVC_DEVICE_PICS" --nodeId "$NODEID" --endpoint "$RVC_DEVICE_ENDPOINT" &&
+./scripts/tests/chipyaml/chiptool.py tests Test_TC_CLOPSTATE_1_1 --PICS "$CLOSURES_DEVICE_PICS" --nodeId "$NODEID" --endpoint "$CLOSURES_DEVICE_ENDPOINT" &&
     echo done
